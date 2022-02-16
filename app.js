79102a392f30cf50d7cb590540172c020309f6b8
andriol <keli87aj@hotmail.com>
2022-02-16 11:37:16 +0200

    final commit

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Die from "./Die";
 import { v4 as uuidv4 } from "uuid";
+import Confetti from "react-confetti";
 
 const App = () => {
   const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
 
+  function checkIfHeld(dices) {
+    const dicesHeld = dices.every((die) => die.isHeld);
+    return dicesHeld;
+  }
+
+  const sameDieValue = (dices) => {
+    const firstValue = dice[0].value;
+    const dicesValue = dices.map((die) => die.value === firstValue);
+    return dicesValue;
+  };
+
+  useEffect(() => {
+    const allDiceHeld = checkIfHeld(dice);
+    const sameDiceValue = sameDieValue(dice);
+
+    if (allDiceHeld && sameDiceValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
+  function generateDice() {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: uuidv4(),
+    };
+  }
+  // generate dice object and pus into array
   function allNewDice() {
     const diceArray = [];
-
     for (let i = 0; i < 10; i++) {
-      const generateDice = {
-        value: Math.floor(Math.random() * 6) + 1,
-        isHeld: false,
-        id: uuidv4(),
-      };
-      diceArray.push(generateDice);
+      diceArray.push(generateDice());
     }
     return diceArray;
   }
+  // roll dice when button clicked
   function rollDice() {
-    setDice(allNewDice());
+    if (!tenzies) {
+      setDice((prev) => {
+        return prev.map((die) => {
+          return die.isHeld ? die : generateDice();
+        });
+      });
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
   }
+  // change property isHeld to true when clicked
   function isDieHeld(id) {
     setDice((prev) => {
       return prev.map((die) => {
@@ -42,9 +77,10 @@ const App = () => {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <div className="dice-container">{displayDice}</div>
       <button onClick={rollDice} className="roll-dice">
-        Roll
+        {tenzies ? "New Game" : "Roll"}
       </button>
     </main>
   );
